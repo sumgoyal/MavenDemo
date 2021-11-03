0ec7638f12304eaeb91893249d272b8268c963da
Sumit <Sumitgoyal0927@gmail.com>
2021-11-03 14:09:26 +0530

    first git commit for project

---
+++ src/test/java/com/qa/test/DemoTest.java
@@ -0,0 +1,42 @@
+package com.qa.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class DemoTest {
+	
+
+	
+	@Test
+	public void sum() {
+		
+		System.out.println("Sum Method");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(30, a+b);
+	}
+	
+	@Test
+	public void sub() {
+		
+		System.out.println("sub Method");
+
+		int a=10;
+		int b=20;
+		Assert.assertEquals(22, b-a);
+	}
+	
+	@Test
+	public void mul() {
+		
+		System.out.println("mul Method");
+
+		int a=10;
+		int b=20;
+		Assert.assertEquals(200, a*b);
+	}
+	
+	
+
+}
